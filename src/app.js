c4455a6787512623e82b3644941b20ae765a809d
alsomemespraysnek <calebsjseeman@gmail.com>
2022-04-09 20:01:55 -0700

    Added a event listener to the button to allow for it to create a new text area on click

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const notes = [
-  { 
-    title: "first note", 
-    noteBody: "this is an example note",
-    id: 1 
-  }
+{ 
+  title: "first note", 
+  noteBody: "this is an example note",
+  id: 1 
+}
 ]
 
-const hi = 1
+const newNoteArea = document.querySelector(".write-note-area")
+const button = document.querySelector(".icons")
+button.addEventListener("click", (evt) => {
+  newNoteArea.innerHTML = "<textarea></textarea>"
+  console.log(newNoteArea.innerHTML)
+})
