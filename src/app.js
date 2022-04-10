3c240a9e639de2c8030b8c8515c6ec4b14ca47f0
Caleb Seeman <caleb@seeman.com>
2022-04-10 13:21:18 -0700

    Added a function that creates a new note as well as began working on getting the title of the note

--- src/app.js
+++ src/app.js
@@ -6,4 +6,20 @@ const notes = [
   }
 ]
 
-const hi = 1
+function getNoteText(){
+  const noteObject = document.getElementsById("note")
+  const note = noteObject.value
+  return note
+}
+
+function getNoteTitle(){
+  const noteText = getNoteText
+  return (noteText.split("\n"))[0]
+}
+
+const noteArea = document.querySelector(".write-note-area")
+const btn = document.querySelector(".icons")
+btn.addEventListener("click", (evt) => {
+  noteArea.innerHTML = "<textarea rows='20' cols='40' id='note'></textarea><button id=save>save</button><button id=delete>delete</button>"
+})
+
